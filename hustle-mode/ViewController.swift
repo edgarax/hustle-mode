7d22a7397d56d8d8c6ccfce410989e53a2144833
Erendira Castillo <eris@macbook-air-de-erendira.local>
2018-10-25 18:28:19 -0500

    Initial Commit

---
+++ hustle-mode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hustle-mode
+//
+//  Created by Erendira Cruz Castillo on 10/25/18.
+//  Copyright © 2018 Erendira Cruz Castillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
